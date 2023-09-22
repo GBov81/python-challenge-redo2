ae90e81424bb21710e199abfd5e37a848fa5616b
GBov81 <gbovie@hotmail.ca>
2023-09-22 19:08:02 -0400

    Update Module 3.02 redone again.py

--- PyPoll/main.py/Module 3.02 redone again.py
+++ PyPoll/main.py/Module 3.02 redone again.py
@@ -1,13 +1,25 @@
+# Import the pandas library
 import pandas as pd
 
+# Read the election data from a URL and store it in a DataFrame
 df = pd.read_csv('https://raw.githubusercontent.com/GBov81/python-challenge-redo2/main/PyPoll/Resources/election_data.csv')
 
-print("Election Results:") 
+# Print the header for the election results
+print("Election Results:")
 print("------------------------------")
+
+# Calculate and print the total number of votes
 print("Total votes:", df['Candidate'].count())
 print("------------------------------")
+
+# Calculate and print the percentage and count of votes for Diana DeGette
 print(f"Diana DeGette: {round(df['Candidate'].str.contains('Diana').sum() * 100 / df['Candidate'].count(), 3)}%, ({df['Candidate'].str.contains('Diana').sum()})")
+
+# Calculate and print the percentage and count of votes for Raymon Anthony Doane
 print(f"Raymon Anthony Doane: {round(df['Candidate'].str.contains('Ray').sum() * 100 / df['Candidate'].count(), 3)}%, ({df['Candidate'].str.contains('Ray').sum()})")
+
 print("------------------------------")
+
+# Find and print the winner by identifying the candidate with the most votes
 print("Winner:", max(df['Candidate'], key=df['Candidate'].value_counts().get))
 print("------------------------------")
